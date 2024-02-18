3234ebfe7e4fb686ba2e5fb883aac7976d74230c
DannyDeng <1041422509@qq.com>
2024-02-18 14:42:22 -0800

    Add Copyright

--- src/temporal_sampling/__init__.py
+++ src/temporal_sampling/__init__.py
@@ -1,3 +1,7 @@
+#  Copyright (c) Meta, Inc. and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
 from .src.temporal_sampling.searchsorted import sample_with_pad
 from .src.temporal_sampling.searchsorted import SEARCHSORTED_GPU_AVAILABLE
 from .src.temporal_sampling.searchsorted import SEARCHSORTED_CPU_AVAILABLE
--- src/temporal_sampling/setup.py
+++ src/temporal_sampling/setup.py
@@ -1,3 +1,7 @@
+#  Copyright (c) Meta, Inc. and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
 from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDA_HOME
 from torch.utils.cpp_extension import CppExtension, CUDAExtension
